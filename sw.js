e7c4aef24efedf74218122c7afac5d28d650b245
Annika <annika@eckhardt@gmx.de>
2026-09-02 17:05:35 +0200

    initial version

---
+++ sw.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = "monsterjaeger-v1";
+
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => cache.addAll(["./", "./manifest.json"])),
+    );
+    self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+    event.waitUntil(
+        caches.keys().then((keys) =>
+            Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))),
+        ),
+    );
+    self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+    if (event.request.method !== "GET" || !event.request.url.startsWith(self.location.origin)) {
+        return;
+    }
+
+    event.respondWith(
+        caches.match(event.request).then((cachedResponse) => {
+            if (cachedResponse) {
+                return cachedResponse;
+            }
+
+            return fetch(event.request).then((networkResponse) => {
+                const responseToCache = networkResponse.clone();
+                void caches.open(CACHE_NAME).then((cache) => cache.put(event.request, responseToCache));
+                return networkResponse;
+            });
+        }),
+    );
+});
